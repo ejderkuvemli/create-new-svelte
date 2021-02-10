f2156118193524ce09ed97d91bcccd6bea8d0151
Ejder Kuvemli <ekuvemli@hotmail.com>
2021-02-10 15:19:06 +0300

    added auto-npm install

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const fs = require('fs-extra');
 const path = require('path');
 const os = require('os');
+const child_process = require('child_process');
 
 const args = process.argv.slice(2);
 const appName = args[0];
 const root = path.resolve(appName);
-
-const packageJson = {
-    name: appName,
-    version: '0.0.1',
-    private: true,
-};
+const templateFolder = path.join(__dirname,'templates/default');
+const packageJson = JSON.parse(fs.readFileSync(path.join(templateFolder, 'package.json')));
+packageJson.name = appName;
+packageJson.version = '0.1.0';
 
 fs.ensureDirSync(appName);
-
+fs.copySync(templateFolder, appName);
 fs.writeFileSync(
     path.join(root, 'package.json'),
     JSON.stringify(packageJson, null, 2) + os.EOL
 );
 
+child_process.execSync('npm install --prefix ' + appName, {stdio:[0,1,2]} );
